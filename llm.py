be7d0cdc59ea27a9857de786276aa5360370d206
Abdul Wasiue <channarwasiue@yahoo.com>
2025-03-27 02:35:23 +0500

    Implemented document reader

--- llm.py
+++ llm.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7daa382572fd8281789861eec0d1c0b1fcc303dbeb8af8f6e0480946a152d4f
-size 2688
+oid sha256:fc294cb84d95ffc87558149424bb2b66a1c4004d4232c387243fd73c62254a20
+size 5097
